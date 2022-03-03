3f366252524e784eed1db8c8b132d79c49f3d52b
mehadi1720 <mehedihasanrana.17203087@gmail.com>
2022-03-03 22:53:58 +0600

    template setup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin1', function () {
+    return view('admin');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
